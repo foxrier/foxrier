9b6c4dc8eae36279db59ad082efd8e59effbac46
狐里叶 <46236730+foxrier@users.noreply.github.com>
2018-12-31 13:50:51 +0800

    Update typecho.sh

--- typecho.sh
+++ typecho.sh
@@ -21,11 +21,11 @@ tar -zxvf 1.1-17.10.30-release.tar.gz
 zypper install -y nginx mariadb php7 php7-fpm php7-gd php7-mysql php7-curl \
        php7-mbstring php7-iconv 
 
-mkdir /usr/share/nginx/blog
+mkdir /usr/share/nginx/typecho
 
-rsync -avP build/ /usr/share/nginx/blog/
+rsync -avP build/ /usr/share/nginx/typecho/
 
-chown -R nginx:nginx /usr/share/nginx/blog
+chown -R nginx:nginx /usr/share/nginx/typecho
 
 cp /root/php.ini /etc/php7/fpm/
 
